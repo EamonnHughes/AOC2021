361e8e67142180f8ed691d8d8756273ad367a3dd
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-12-19 18:38:56 -0500

    First commit.

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,16 @@
+package aoc21
+
+object Day1 extends App {
+  println("hello world")
+  var lLength = 10
+  var nOfHigher = 0
+
+  for (i <- 0 to lLength) {
+
+    if (i == 5) {
+      println(i)
+    }
+
+  }
+
+}
