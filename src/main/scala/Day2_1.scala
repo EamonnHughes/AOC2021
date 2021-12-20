46d59a4a10f5c2ca5bfb60b57877c5325e3336bd
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-12-20 15:18:58 -0500

    Update Day2_1.scala

--- src/main/scala/Day2_1.scala
+++ src/main/scala/Day2_1.scala
@@ -1,3 +1,16 @@
 package aoc21
 
-object Day2_1 extends App {}
+import scala.io.Source
+
+object Day2_1 extends App {
+  val lList = Source.fromFile("inputAOCD1.1.txt").getLines.toList
+  val lLength = lList.length
+
+  var pX = 0
+  var pY = 0
+  var total = 0
+
+  for (i <- 0 to lLength - 1) {}
+  println(total)
+
+}
