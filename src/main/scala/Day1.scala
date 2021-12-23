cc28948fe834d93e9c33d8f907f06f303b926025
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-12-23 12:08:56 -0500

    day 2_2 correct

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -2,20 +2,8 @@ package aoc21
 import scala.io.Source
 
 object Day1 extends App {
-  println("hello eamonn(the best person)")
-  var nOfHigher = 0
 
   val lList =
     Source.fromFile("inputAOCD1.1.txt").getLines.toList
-  val lLength = lList.length
-
-  for (i <- 0 to lLength - 2) {
-    val nFirst = lList(i).toInt
-    val nSecond = lList(i + 1).toInt
-    if (nFirst < nSecond) {
-      nOfHigher += 1
-    }
-  }
-  println(nOfHigher)
 
 }
--- src/main/scala/Day3_2.scala
+++ src/main/scala/Day3_2.scala
@@ -4,33 +4,14 @@ import scala.io.Source
 
 object Day3_2 extends App {
   val lList = Source.fromFile("inputAOC3.1.txt").getLines.toList
-  var MoCom = ""
-  var cList = lList
 
-  for (i <- 0 to 11) {
+  def whichDigit(index: Int): Char = {
 
-    var numX = 0
-    var numY = 0
-    var numZ = ' '
-    for (y <- 0 to cList.length - 1) {
-
-      if (cList(y).charAt(i) == '0') {
-        numX += 1
-      } else {
-        numY += 1
-      }
-
-    }
-    if (numX > numY) {
-      numZ = '0'
-    } else {
-      numZ = '1'
-    }
-    cList = cList.filter(linX => linX.charAt(i) == numZ)
+    var higherNum = ' '
+    val nOf1 = lList.count(str => str.charAt(index) == '1')
+    return higherNum
 
   }
+  println()
 
-  val ls = Integer.parseInt(cList(0), 2).toInt
-  println(cList)
-  println(ls)
 }
