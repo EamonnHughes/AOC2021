6e0edd6af5687d61caa158736dbc8935393454c9
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-12-24 14:22:18 -0500

    day 4_1

--- src/main/scala/Day4_1.scala
+++ src/main/scala/Day4_1.scala
@@ -3,7 +3,11 @@ package aoc21
 import scala.io.Source
 
 object Day4_1 extends App {
-  val lList = Source.fromFile("AOCD4.txt").getLines.toList
+  val lIter = Source.fromFile("AOCD4.txt").getLines
+  val firstLine = lIter.next()
+  val lList = lIter.toList
+  val Input = firstLine.split(",")
+  println(Input.mkString(","))
   println(lList)
 
 }
