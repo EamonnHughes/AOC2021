90afcef28b32a107155d88a314d2b667da32ae8d
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-12-28 19:06:26 -0500

    Create Invest.scala

---
+++ src/main/scala/Invest.scala
@@ -0,0 +1,16 @@
+package aoc21
+
+object Invest extends App {
+
+  var mo = 01
+  var invest = 0.0
+  var depos = 1000.0
+  var perc = .1
+  while (mo <= 480) {
+    invest += depos
+    invest += invest * perc / 12
+    mo += 1
+  }
+  println(invest.toInt)
+  println((invest * (.06/12)).toInt)
+}
