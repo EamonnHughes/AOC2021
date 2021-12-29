1bc4239351ea52d7924c459db51fc28b17b6ae54
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-12-29 18:41:59 -0500

    Update Day4_1.scala

--- src/main/scala/Day4_1.scala
+++ src/main/scala/Day4_1.scala
@@ -2,21 +2,36 @@ package aoc21
 
 import scala.io.Source
 
+val Indices = (0 to 4).toList
+
+case class Board(rows: List[List[Int]]) {
+  def check(Bingoed: List[Int]): Boolean = {
+
+      Indices.exists(
+      rows.forall(row => Bingoed.contains(row()))
+      )
+      rows.exists(row => row.forall(int => Bingoed.contains(int)))
+  }
+}
+
 object Day4_1 extends App {
   val lIter = Source.fromFile("AOCD4.txt").getLines
   val firstLine = lIter.next()
-  val lList = lIter.toList
-  val Input = firstLine.split(",")
+  val lList = lIter.toList.filter(str => str != "")
+  val RowList =
+    lList.map(item => item.trim.split(" +").toList.map(s => s.toInt))
+  val BoardList = RowList.grouped(5).toList.map(item => new Board(item))
+  val Input = firstLine.split(",").map(s => s.toInt)
   var index = 0
-  // while no bingo
-  while (index < Input.length) {
+  var bingoed: List[Int] = List()
+  while (1 == 1) {
+    val nextNum = Input(index)
 
-    println(Input(index))
+    bingoed = nextNum :: bingoed
     index += 1
   }
-  //if bingo
-  if (1 == 1) {
-    println("u " + 1)
+  for (i <- 0 until BoardList.length) {
+    println(BoardList(i))
   }
 
 }
