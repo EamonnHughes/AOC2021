826f760371daceea2ea8e96dd9016a3832f8df72
arathorn2 <43751985+arathorn2@users.noreply.github.com>
2021-12-30 14:21:19 -0500

    Update Day5_1.scala

--- src/main/scala/Day5_1.scala
+++ src/main/scala/Day5_1.scala
@@ -2,7 +2,14 @@ package aoc21
 
 import scala.io.Source
 
+case class lines(first: List[Int], second: List[Int]) {
+  def CheckIntersect(lineAgainst: lines): Boolean = {}
+}
+
 object Day5_1 extends App {
 
-  val lList = Source.fromFile("5_1inp.txt").getLines.toList
+  val lList =
+    Source.fromFile("5_1inp.txt").getLines.toList.map()
+  val Input = lList.split(" -> ").map(s => s.toInt)
+  println("EEEEEEEEE")
 }
